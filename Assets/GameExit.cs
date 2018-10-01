1ae51f2b66fa4f2366b758fc15c94a160cf8739d
David Mendaño <dmendanyo@gmail.com>
2018-10-02 00:24:22 +0200

    Init

---
+++ Assets/GameExit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameExit : MonoBehaviour {
+
+	public void SalirJuego () {
+		Debug.Log("Salir del puñetero juego");
+   	}
+
+}
